688c21534e23f788c83da7555232a60bcd2cb4db
Shreeram <Shreeram@wangoes.com>
2024-11-27 18:20:34 +0530

    .

--- document/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Documents
-
-
-class DocumentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Documents
-        fields = '__all__'
